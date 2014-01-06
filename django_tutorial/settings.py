0518666a1df3b11a26e361e5064a5670883f0515
Courtland Allen <courtlandallen@gmail.com>
2014-01-05 23:47:13 -0800

    Updated time zone (--> PST) and database (--> mysql) settings

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -57,8 +57,11 @@ WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_tutorial_development',
+        'USER': 'courtland',
+        'PASSWORD': 'Bauglir015',
+        'HOST': 'localhost'
     }
 }
 
@@ -67,7 +70,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
